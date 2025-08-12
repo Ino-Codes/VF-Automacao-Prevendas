008fdd111824da448e756b2c481dca0e6e777e10
Inácio de Moraes Lindenmeyer <103116915+Ino-Codes@users.noreply.github.com>
2025-08-12 08:58:24 -0300

    ADD: assets frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Garante que o DOM esteja totalmente carregado antes de executar o código do React
+document.addEventListener("DOMContentLoaded", () => {
+  const rootElement = document.getElementById("root");
+  if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  } else {
+    console.error("Elemento 'root' não encontrado no DOM.");
+  }
+});
