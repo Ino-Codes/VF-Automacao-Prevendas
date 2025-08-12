8c1a8a2140d092c3c4de6ff179e11a607c2838e5
Inácio de Moraes Lindenmeyer <103116915+Ino-Codes@users.noreply.github.com>
2025-08-12 09:01:06 -0300

    DEBUG: alterando código do main.jsx para tentar localizar a origem do erro

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./App.css";
-
-// Garante que o DOM esteja totalmente carregado antes de executar o código do React
-document.addEventListener("DOMContentLoaded", () => {
-  const rootElement = document.getElementById("root");
-  if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
-  } else {
-    console.error("Elemento 'root' não encontrado no DOM.");
-  }
-});
+document.getElementById("root").innerHTML = "<h1>Teste de Renderização</h1>";
